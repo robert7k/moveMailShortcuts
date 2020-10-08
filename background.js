7466f9014ec68cb9d958f8207be727939ce48825
Robert Siebeck <robert@siebeck.org>
2020-10-08 22:21:10 +0200

    refactor background.js

--- background.js
+++ background.js
@@ -1,30 +1,37 @@
-browser.commands.onCommand.addListener(function (command) {
+browser.commands.onCommand.addListener(onCommand);
+
+async function onCommand(command) {
 	if (command !== "moveMail") 
 		return;
-	console.log("Moving Mail");
-	browser.storage.sync.get("folder").then(setting => {
-		console.log("Got saved folder value: " + setting.folder);
-		browser.tabs.query({
-			active: true,
-			currentWindow: true,
-		}).then(tabs => {
-			let tabId = tabs[0].id;
-			browser.messageDisplay.getDisplayedMessage(tabId).then((message) => {
-				findFolder(setting.folder).then(folder => {
-					if (!folder)
-						console.log(`Folder ${setting.folder} not found`);
-					else
-						browser.messages.move([message.id], folder);
-				});
-			});
-		});
-	});
-});
+	console.log("Received command to move mail.");
+	const setting = await browser.storage.sync.get("folder").catch(console.error);
+	if (!setting || !setting.folder) {
+		console.err("No folder setting found");
+		return;
+	}
+	console.log("Got saved folder value: " + setting.folder);
 
-function findFolder(path) {
-	return browser.accounts.list().then(accounts => {
-		return findSubfolder(path, accounts.flatMap(account => account.folders));
+	const tabs = await browser.tabs.query({
+		active: true,
+		currentWindow: true,
 	});
+	let tabId = tabs[0].id;
+	const message = await browser.messageDisplay.getDisplayedMessage(tabId);
+	if (!message) {
+		console.log("No message selected");
+		return;
+	}
+	const folder = await findFolder(setting.folder);
+	if (!folder) {
+		console.log(`Folder ${setting.folder} not found`);
+		return;
+	}
+	await browser.messages.move([message.id], folder);
+}
+
+async function findFolder(path) {
+	const accounts = await browser.accounts.list();
+	return findSubfolder(path, accounts.flatMap(account => account.folders));
 }
 
 function findSubfolder(path, list) {
